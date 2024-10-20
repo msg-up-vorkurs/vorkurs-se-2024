f2ecc8da3e3b6b54018b8c7b725f1b6056c0d16c
suessfe <Felix.Suess_extern@kvb.de>
2024-10-21 01:11:32 +0200

    Add 02-variables_operators tasks

---
+++ 02_variables-operators/task/VariablesTask.java
@@ -0,0 +1,23 @@
+package task;
+
+public class VariablesTask {
+
+    public static void main(String[] args) {
+
+        /*
+         * TODO Aufgabe 1a: Deklarieren und Initialisieren Sie Variablen für die Beschreibung eines Computers
+         *  (Hersteller, Preis, Farbe, Lüfter, Arbeitsspeicher, etc.).
+         */
+
+        
+
+
+        /*
+         * TODO Aufgabe 1b: Ausgabe der Variablen
+         *  z.B. System.out.println(manufacturer);
+         */
+
+
+
+    }
+}
